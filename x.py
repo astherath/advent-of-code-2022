188018080135b5cb2688f603f597324bbe126d42
astherath <farceriv@gmail.com>
2022-12-06 11:43:14 -0500

    added working script

--- x.py
+++ x.py
@@ -5,17 +5,15 @@ import os
 def create(day: int) -> None:
     """creates a new dir for the AOC day and puts in input too."""
     day_str = ""
-    match len(day):
+    match len(str(day)):
         case 1:
             day_str = f"0{day}"
         case 2:
             day_str = str(day)
     os.system(f"mkdir {day_str}")
+    os.system(f"mv 'input.txt' {day_str}")
 
-    input_url = f"https://adventofcode.com/2022/day/{day}/input"
-    os.system(f'wget "{input_url}" "input.txt"')
-
-    os.system(f"chdir {day_str}")
+    os.system(f"cd {day_str}")
 
 if __name__ == '__main__':
     fire.Fire(create)
