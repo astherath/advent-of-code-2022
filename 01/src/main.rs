79df2e764a1dac0e6aad9167da5c80ad8fdf17f4
astherath <farceriv@gmail.com>
2022-12-06 12:51:38 -0500

    first day solved

--- 01/src/main.rs
+++ 01/src/main.rs
@@ -1,3 +1,20 @@
+use common::read_input;
 fn main() {
-    println!("Hello, world!");
+    let path = "input.txt";
+    let lines = read_input(path).expect("couldnt open file");
+    let mut counter = 0;
+    let mut nums = vec![];
+    for line in lines {
+        if line.len() == 0 {
+            nums.push(counter);
+            counter = 0;
+        } else {
+            counter += line.parse::<usize>().expect("couldn't parse");
+        }
+    }
+
+    nums.sort();
+    let max: usize = nums.into_iter().rev().take(3).sum();
+
+    println!("max: {}", max);
 }
